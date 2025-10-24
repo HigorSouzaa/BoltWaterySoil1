f6ff4f923b9aa52e853e65d98b50d508338e4c66
HigorSouzaa <higorhenry102@gmail.com>
2025-10-24 17:18:44 -0300

    Disable email input field in UserSettings component

--- src/components/UserSettings.tsx
+++ src/components/UserSettings.tsx
@@ -432,6 +432,7 @@ export const UserSettings: React.FC = () => {
                         }
                         className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                         placeholder="seu@email.com"
+                        disabled
                       />
                     </div>
 
